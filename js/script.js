e7383cc3f54b00703bc1793d80e8810b7aa93929
Paweł Stolecki <noigell@gmail.com>
2018-03-18 14:18:39 +0100

    zadanie-10-1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+	$("span:even").css("color", "red");
+	var paragraphs = $("p");
+
+	paragraphs.each(function(index, element) {
+		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+		$(element).append(button);
+	});
+
+	$(".btn").click(function(){
+		alert($(this).attr("data-tmp"));
+	});
+});
